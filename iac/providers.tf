5c50df4a80f9051294003dc1e6ae899063c03b93
Célia Vytrac <celia.j.vytrac@gmail.com>
2023-11-16 19:46:31 -0500

    add talos as provider

--- iac/providers.tf
+++ iac/providers.tf
@@ -5,6 +5,10 @@ terraform {
   }
 
   required_providers {
+    talos = {
+      source = "siderolabs/talos"
+      version = "~> 0.4.0-alpha.0"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
